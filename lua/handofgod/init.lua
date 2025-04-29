85f96b3b4f024f09a02933a67942cb1adf415f3b
Alucherdi <alucherdi@gmail.com>
2025-04-28 19:03:05 -0600

    feat(init): adding gen_list function

--- lua/handofgod/init.lua
+++ lua/handofgod/init.lua
@@ -11,15 +11,39 @@ local function split(inputstr, sep)
     return t
 end
 
+local function gen_list()
+    local files = {}
+    local directories = {}
+
+    local bufferPath = vim.fn.expand('%:p:h')
+    local list = vim.fn.globpath(bufferPath, '*', false, true)
+
+    for _, item in pairs(list) do
+        local stat = vim.loop.fs_stat(item)
+        if stat then
+            if stat.type == 'file' then
+                table.insert(files, vim.fn.fnamemodify(item, ':.'))
+            end
+
+            if stat.type == 'directory' then
+                table.insert(directories, vim.fn.fnamemodify(item, ':.') .. '/')
+            end
+        end
+    end
+
+    return {
+        files = files,
+        directories = directories
+    }
+end
+
 function M.create_window()
+    local items = gen_list()
+    local list = vim.tbl_extend('keep', items.directories, items.files)
+
     M.buffer = vim.api.nvim_create_buf(false, true)
 
-    local path = vim.fs.root(0, vim.fn.expand('%')) or ""
-    path = path:gsub(vim.uv.cwd(), '.')
-
-    M.initial_files = split(vim.fn.glob(path .. '/*'), '\r\n')
-
-    vim.api.nvim_buf_set_lines(M.buffer, 0, 1, false, M.initial_files)
+    vim.api.nvim_buf_set_lines(M.buffer, 0, 1, false, list)
 
     M.window = vim.api.nvim_open_win(M.buffer, true, {
         border   = "rounded",
@@ -29,6 +53,9 @@ function M.create_window()
         row = 0,
         col = math.ceil(vim.o.columns / 3),
     })
+end
+
+function M:open()
 end
 
 return M
