234c390c5b08ce37c8b4465e160cbcbd49d7c2d5
Alucherdi <hjgo127942@gmail.com>
2025-05-12 01:59:31 -0600

    fix(manager): window title and hidden files
    now hidden files are shown.
    when going to cwd instead of showing full path in window title it shows './'.

--- lua/handofgod/manager/init.lua
+++ lua/handofgod/manager/init.lua
@@ -7,20 +7,25 @@ local function gen_list(path)
     local files = {}
 
     M.bufferPath = path or vim.fn.expand('%:p:h')
-    local list = vim.fn.globpath(M.bufferPath, '*', false, true)
+
+    local hidden = vim.fn.globpath(M.bufferPath, '.*', false, true)
+    local normal = vim.fn.globpath(M.bufferPath, '*', false, true)
+    local list = vim.list_extend(normal, hidden)
+
 
     for _, item in pairs(list) do
         local stat = vim.loop.fs_stat(item)
         local rel = vim.fn.fnamemodify(item, ':t')
+        if rel == '.' or rel == '..' then goto skip end
+
         if stat then
             if stat.type == 'directory' then
                 table.insert(files, 1, rel .. '/')
-            end
-
-            if stat.type == 'file' then
+            else
                 table.insert(files, rel)
             end
         end
+        ::skip::
     end
 
     return files
@@ -34,15 +39,24 @@ function M:open()
     local buf = vim.api.nvim_create_buf(false, true)
     vim.api.nvim_buf_set_lines(buf, 0, -1, false, list)
 
-    local window = commons:create_window(
-        vim.fn.fnamemodify(M.bufferPath, ':.'),
-        buf)
+    local title = vim.fn.fnamemodify(M.bufferPath, ':.')
 
+    if title == vim.uv.cwd() then
+        title = './'
+    end
+
+    local window = commons:create_window(
+        title,
+        buf)
 
     vim.keymap.set('n', '<BS>', function()
         list = gen_list(vim.fn.fnamemodify(M.bufferPath, ':h'))
+        local title = vim.fn.fnamemodify(M.bufferPath, ':.')
+        if title == vim.uv.cwd() then
+            title = './'
+        end
         vim.api.nvim_buf_set_lines(buf, 0, -1, false, list)
-        vim.api.nvim_win_set_config(window, {title = vim.fn.fnamemodify(M.bufferPath, ':.')})
+        vim.api.nvim_win_set_config(window, {title = title})
     end)
 
     vim.keymap.set('n', '<leader>u', function()
@@ -91,9 +105,13 @@ end
 
 function M:goto(path, buf, window)
     local list = gen_list(M.bufferPath .. '/' .. path:sub(0, -2))
+    local title = vim.fn.fnamemodify(M.bufferPath, ':.')
+    if title == vim.uv.cwd() then
+        title = './'
+    end
 
     vim.api.nvim_buf_set_lines(buf, 0, -1, false, list)
-    vim.api.nvim_win_set_config(window, {title = vim.fn.fnamemodify(M.bufferPath, ':.')})
+    vim.api.nvim_win_set_config(window, {title = title})
 end
 
 function M:edit(path)
