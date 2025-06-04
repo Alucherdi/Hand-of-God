ccac01454fd2beb764350821815f5d13b895dcde
Alucherdi <hjgo127942@gmail.com>
2025-06-03 22:13:17 -0600

    feat(finder): using filetype api implementation to optimize syntax search

--- lua/handofgod/data/filetype.lua
+++ lua/handofgod/data/filetype.lua
@@ -2,16 +2,7 @@ local M = {}
 M.cache = {}
 
 function M.detect(path)
-    if M.cache[path] then return M.cache[path] end
-    local buf = vim.fn.bufadd(path)
-    vim.bo[buf].bufhidden = 'wipe'
-    vim.bo[buf].swapfile = false
-    vim.fn.bufload(buf)
-
-    local ft = vim.bo[buf].filetype
-    vim.api.nvim_buf_delete(buf, {force = true})
-    M.cache[path] = ft or 'plaintext'
-    return M.cache[path]
+    return vim.filetype.match({filename = path}) or 'plaintext'
 end
 
 return M
